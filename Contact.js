13b7bbc95bc6bbb841e1aced4a149cde4af6eac0
deeyadas <102305510+deeyadas@users.noreply.github.com>
2023-07-10 15:54:58 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+    <h1>Contact Details:94567891456
+    MOBILE NUMBER: 0824-98451462
+    </h1>
+    </>
+  )
+}
+
+export default Contact
---
+++ Destination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Destination = () => {
+  return (
+    <div>
+      {/* Your destination page content here */}
+    </div>
+  );
+};
+
+export default Destination;
